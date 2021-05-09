42f23232ced8be2743e3f50f9975ac7ed5642d38
yoheisenju <yohei.senju@gmail.com>
2021-05-09 22:40:48 +0900

    Change package name

--- src/mem/init.go
+++ src/mem/init.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
+var Cache *memcache.Client = InitCache("127.0.0.1", "11211")
 
 func InitCache(host string, port string) *memcache.Client {
     endpoint := host + ":" + port
